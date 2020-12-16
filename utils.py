9a3189d04f29dd8900adda6e11ba7b0686dad6a7
Emre Ulgac <ulgacemre@gmail.com>
2020-12-16 18:33:28 +0100

    util

--- utils.py
+++ utils.py
@@ -50,46 +50,8 @@ def open_link(url, new_tab=True):
 
 
 def download_button(
-    object_to_download, download_filename, button_text  # , pickle_it=False
+    object_to_download, download_filename, button_text 
 ):
-    """
-    Generates a link to download the given object_to_download.
-    
-    From: https://discuss.streamlit.io/t/a-download-button-with-custom-css/4220
-    Params:
-    ------
-    object_to_download:  The object to be downloaded.
-    download_filename (str): filename and extension of file. e.g. mydata.csv,
-    some_txt_output.txt download_link_text (str): Text to display for download
-    link.
-    button_text (str): Text to display on download button (e.g. 'click here to download file')
-    pickle_it (bool): If True, pickle file.
-    Returns:
-    -------
-    (str): the anchor tag to download object_to_download
-    Examples:
-    --------
-    download_link(your_df, 'YOUR_DF.csv', 'Click to download data!')
-    download_link(your_str, 'YOUR_STRING.txt', 'Click to download text!')
-    """
-    # if pickle_it:
-    #     try:
-    #         object_to_download = pickle.dumps(object_to_download)
-    #     except pickle.PicklingError as e:
-    #         st.write(e)
-    #         return None
-
-    # else:
-    #     if isinstance(object_to_download, bytes):
-    #         pass
-
-    #     elif isinstance(object_to_download, pd.DataFrame):
-    #         object_to_download = object_to_download.to_csv(index=False)
-
-    #     # Try JSON encode for everything else
-    #     else:
-    #         object_to_download = json.dumps(object_to_download)
-
     try:
         # some strings <-> bytes conversions necessary here
         b64 = base64.b64encode(object_to_download.encode()).decode()
@@ -131,6 +93,5 @@ def download_button(
         custom_css
         + f'<a download="{download_filename}" id="{button_id}" href="data:file/txt;base64,{b64}">{button_text}</a><br><br>'
     )
-    # dl_link = f'<a download="{download_filename}" id="{button_id}" href="data:file/txt;base64,{b64}"><input type="button" kind="primary" value="{button_text}"></a><br></br>'
 
     st.markdown(dl_link, unsafe_allow_html=True)
