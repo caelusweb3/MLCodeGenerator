1c95b8b5d608acc0aa802c0ee312508ea8454e1d
Emre Ulgac <ulgacemre@gmail.com>
2020-12-16 16:31:40 +0100

    MLCodeGenerator

---
+++ setup.sh
@@ -0,0 +1,10 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"ulgacemre@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
